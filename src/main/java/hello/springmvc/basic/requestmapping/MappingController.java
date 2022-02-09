acb9db27ef54bcf423b57b74edf034dea57216cf
Kim9xd <kim9xd@gmail.com>
2022-02-09 11:58:10 +0900

    * 스프링 MVC - 기본 기능   - 요청 매핑 - API 예시

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
